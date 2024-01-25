b3903e4f521382db709b8ea4fa6be8a1ea6c51ba
gchurakov <gachurakov@edu.hse.ru>
2024-01-26 00:41:31 +0500

    post request with data display

--- picture-transform-asp.net_core-react/ClientApp/src/components/Home.js
+++ picture-transform-asp.net_core-react/ClientApp/src/components/Home.js
@@ -1,83 +1,99 @@
 import React, { Component } from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
     constructor(props) {
         super(props);
-        this.state = { url_list: []};
+        this.state = { uri_list: [], loading: false };
     }
 
-    // componentDidMount() {
-    //     this.fragmentsRequest();
-    // }
-
-
-    static renderTable(url_list) {
-        // let q = url_list.resize()
+    static renderList(uri_list) {
         
+        let urls = uri_list.map(uri=>"."+uri);
+        
+        let content = urls.map((url, index)=> 
+                <li><img src={url} alt={`Fragment ${index+1}`} style={{width: 'initial', height: 'initial', padding: '10px'}}/></li>);
         return (
-            <table className="table" aria-labelledby="tableLabel">
-                <tbody>
-                {/*TODO : create columns and rows*/}
-                
-                {/*{url_list.map(url =>*/}
-                {/*    <tr>*/}
-                {/*        <p>{url}</p>*/}
-                {/*        /!*<img src={url} alt={url}/>*!/*/}
-                {/*    </tr>*/}
-                {/*)}*/}
-                </tbody>
-            </table>
+            <ul style={{listStyleType: 'none'}}>
+                {content}
+            </ul>
         );
     }
 
+    onClickFragmentsRequest = async (event) => {
+        event.preventDefault();
+
+        const rows = Number(document.getElementById("rows").value);
+        const columns = Number(document.getElementById("columns").value);
+        const url = document.getElementById("url").value;
+
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Request-Method': 'POST',
+                'Access-Control-Request-Headers': 'Content-Type' 
+            },
+            body: JSON.stringify({ "Rows": rows, "Columns": columns, "ImageUrl": url })
+        };
+
+        this.setState({ loading: true });
+
+        await fetch('https://127.0.0.1:7226/api/PictureTransform', requestOptions)
+            .then(response => response.json())
+            .then(data => this.setState({ uri_list: data, loading: false }))
+            .catch(er => console.log(er), event.preventDefault());
+    };
+
     render() {
-        let contents = this.state.loading
-            ? <p></p>
-            : Home.renderTable(this.state.uri_list);
+        const contents = this.state.loading
+            ? <p>Loading...</p>
+            : Home.renderList(this.state.uri_list);
 
         return (
             <div>
                 <h1>Тестовое задание: нарезка изображения.</h1>
-                <h3>О приложении:</h3>
-                <ul>
-                    <li>Backend - ASP.Net Core</li>
-                    <li>Frontend - React</li>
-                    <li><a href="https://shorturl.at/hzRSU">Задание</a></li>
-                    <li><a href="https://github.com/gchurakov">Репозиторий проекта на Github</a></li>
-                </ul>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>
+                                <h3>О приложении:</h3>
+                            </td>
+                            <td>
+                                <h3>Что из ТЗ выполнено:</h3>
+                            </td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>
+                            <ul>
+                            <li>Backend - ASP.Net Core</li>
+                                    <li>Frontend - React</li>
+                                    <li><a href="https://shorturl.at/hzRSU">Задание</a></li>
+                                    <li><a href="https://github.com/gchurakov">Github</a></li>
+                                </ul>
+                            </td>
+                            <td>
+                                <ul>
+                                    <li> Написано апи на C#</li>
+                                </ul>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
                 <h3>Отправить запрос</h3>
-                <form>
-                    <p>Количество разрезов по вертикали: <input type="text" id="columns" required/></p>
-                    <p>Количество разрезов по горизонтали: <input type="text"  id="rows" required/></p>
-                    <p>Ссылка на изображение: <input type="text"  id="url" required/></p>
-                    <button type="submit"  onClick={Home.onClickfragmentsRequest}>Отправить</button>
+                <form onSubmit={this.onClickFragmentsRequest}>
+                    <p>Количество разрезов по вертикали: <input type="text" id="columns" required /></p>
+                    <p>Количество разрезов по горизонтали: <input type="text" id="rows" required /></p>
+                    <p>Ссылка на изображение: <input type="text" id="url" required /></p>
+                    <button type="submit">Отправить</button>
                     <button type="reset">Очистить</button>
                 </form>
-                <p id="request"></p>
-                <p id="responce"></p>
                 {contents}
             </div>
         );
-    }
-
-    async onClickfragmentsRequest() {
-        this.rows = Number(document.getElementById("rows").textContent);
-        this.columns = Number(document.getElementById("columns").textContent);
-        let url = document.getElementById("url").textContent;
-
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ "Rows": this.rows, "Columns": this.columns, "ImageUrl": url })
-        };
-
-        document.getElementById("request").textContent = requestOptions;
-        
-        await fetch('/api/PictureTransform', requestOptions)
-            .then(response => response.json())
-            .then(data => this.setState({uri_list: data, loading: false}));
-
     }
 }
