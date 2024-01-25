edb13a107419ed8c7c77029140fbb4874839013a
gchurakov <gachurakov@edu.hse.ru>
2024-01-26 00:40:29 +0500

    rm user img dir and allow reat

--- picture-transform-asp.net_core-react/Program.cs
+++ picture-transform-asp.net_core-react/Program.cs
@@ -1,3 +1,5 @@
+Directory.Delete(Path.Combine("ClientApp", "public", "image-fragments"),true); 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +14,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseCors(builder =>
+{
+    builder.WithOrigins("https://127.0.0.1:44432")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
